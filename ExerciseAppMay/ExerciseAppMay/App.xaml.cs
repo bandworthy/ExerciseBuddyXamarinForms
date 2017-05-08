67390a5a31fd7e5308fe5a49ead9b37ec82f56b7
bandworthy <s3240684@student.rmit.edu.au>
2017-05-08 15:26:16 +1000

    Add project files.

---
+++ ExerciseAppMay/ExerciseAppMay/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ExerciseAppMay
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            //MainPage = new ExerciseAppMay.MainPage();
+            //MainPage = new Exercise();
+            MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ ExerciseAppMay/ExerciseAppMay/Exercise.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExerciseAppMay
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Exercise : ContentPage
+	{
+		public Exercise ()
+		{
+			InitializeComponent ();
+
+            IntensityPicker.Items.Add(" Low");
+            IntensityPicker.Items.Add(" Medium");
+            IntensityPicker.Items.Add(" High");
+            IntensityPicker.SelectedIndex = 1;
+
+
+            DurationPicker.Items.Add("20 Seconds");
+            DurationPicker.Items.Add("25 Seconds");
+            DurationPicker.Items.Add("30 Seconds");
+            DurationPicker.Items.Add("35 Seconds");
+            DurationPicker.Items.Add("40 Seconds");
+            DurationPicker.Items.Add("50 Seconds");
+            DurationPicker.Items.Add("60 Seconds");
+            DurationPicker.SelectedIndex = 0;
+        }
+
+        private void IntensityPicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+           var name = IntensityPicker.Items[IntensityPicker.SelectedIndex];
+
+            DisplayAlert(name, "Selected value", "OK");
+        }
+
+        private void DurationPicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var name = DurationPicker.Items[DurationPicker.SelectedIndex];
+
+#if __ANDROID__
+            DisplayAlert(name, "Selected value", "OK");
+#endif
+        }
+
+    }
+}
